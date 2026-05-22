0db8f509744abdff9291600cd0bd6a10f56f0d41
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-05-23 03:20:12 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData.dark(),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Hello from iPhone!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
